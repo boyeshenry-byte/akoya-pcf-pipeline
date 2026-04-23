c4abd51cba61eab55774e461302c05ae4d8b6cf9
boyeshenry-byte <boyeshenry@gmail.com>
2026-04-23 11:46:24 -0400

    feat: project filtering added to reset_project

--- reset_project.py
+++ reset_project.py
@@ -10,38 +10,58 @@ parser = argparse.ArgumentParser(description="Project folder name")
 parser.add_argument("--project", required=True, type=str, help="Enter the project folder name (case sensitive)")
 args = parser.parse_args()
 
-conn = sqlite3.connect(DB_PATH)
-cursor = conn.cursor()
 
-cursor.execute("DELETE FROM cell_intensity")
-cursor.execute("DELETE FROM cell_features;")
-cursor.execute("DELETE FROM channel_stats;")
-cursor.execute("DELETE FROM segmentation_results;")
-cursor.execute("DELETE FROM pipeline_status;")
-cursor.execute("DELETE FROM slides;")
-cursor.execute("DELETE FROM runs;")
+def reset_db(folder_name, cursor):
+    project_filter = f"%{folder_name}%"
 
-conn.commit()
-conn.close()
+    cursor.execute("""DELETE FROM cell_intensity WHERE cell_id IN (
+                        SELECT cell_id FROM cell_features WHERE slide_id IN (
+                            SELECT slide_id FROM slides WHERE file_path LIKE ?));""", (project_filter,))
+    cursor.execute("""DELETE FROM cell_features WHERE slide_id IN (
+                        SELECT slide_id FROM slides WHERE file_path LIKE ?);""", (project_filter,))
+    cursor.execute("""DELETE FROM channel_stats WHERE slide_id IN(
+                        SELECT slide_id FROM slides WHERE file_path LIKE ?);""", (project_filter,))
+    cursor.execute("""DELETE FROM segmentation_results WHERE slide_id IN (
+                        SELECT slide_id FROM slides WHERE file_path LIKE ?);""", (project_filter,))
+    cursor.execute("""DELETE FROM pipeline_status WHERE slide_id IN (
+                        SELECT slide_id FROM slides WHERE file_path LIKE ?);""", (project_filter,))
+    cursor.execute("DELETE FROM slides WHERE file_path LIKE ?;", (project_filter,))
 
-folder_name = args.project
-project_path = f"{ISILON_BASE}/{folder_name}"
 
 
-if os.path.exists(f"{project_path}/anndata"):
-    shutil.rmtree(f"{project_path}/anndata")
-    print(f"{project_path}/anndata reset")
-if os.path.exists(f"{project_path}/masks"):
-    shutil.rmtree(f"{project_path}/masks")
-    print(f"{project_path}/masks reset")
-if os.path.exists(f"{project_path}/phenotyping"):
-    shutil.rmtree(f"{project_path}/phenotyping")
-    print(f"{project_path}/phenotyping reset")
-if os.path.exists(f"{project_path}/qc_pngs"):
-    shutil.rmtree(f"{project_path}/qc_pngs")
-    print(f"{project_path}/qc_pngs reset")
-if os.path.exists(f"{project_path}/spatial"):
-    shutil.rmtree(f"{project_path}/spatial")
-    print(f"{project_path}/spatial reset")
 
-print(f"Reset complete for project: {folder_name}")
+def reset_dir (project_path):
+    if os.path.exists(f"{project_path}/anndata"):
+        shutil.rmtree(f"{project_path}/anndata")
+        print(f"{project_path}/anndata reset")
+    if os.path.exists(f"{project_path}/masks"):
+        shutil.rmtree(f"{project_path}/masks")
+        print(f"{project_path}/masks reset")
+    if os.path.exists(f"{project_path}/phenotyping"):
+        shutil.rmtree(f"{project_path}/phenotyping")
+        print(f"{project_path}/phenotyping reset")
+    if os.path.exists(f"{project_path}/qc_pngs"):
+        shutil.rmtree(f"{project_path}/qc_pngs")
+        print(f"{project_path}/qc_pngs reset")
+    if os.path.exists(f"{project_path}/spatial"):
+        shutil.rmtree(f"{project_path}/spatial")
+        print(f"{project_path}/spatial reset")
+
+if __name__ == "__main__":
+    conn = sqlite3.connect(DB_PATH)
+    cursor = conn.cursor()
+
+    folder_name = args.project
+    project_path = f"{ISILON_BASE}/{folder_name}"
+
+    try:
+        reset_db(folder_name, cursor)
+        conn.commit()
+        conn.close()
+        print("Database reset")
+        reset_dir(project_path)
+        print("Directory reset")
+    except Exception as e:
+        print(e)
+
+    print(f"{folder_name} reset complete")
